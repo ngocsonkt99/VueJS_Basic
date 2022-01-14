cdf348a643f070d642cf87e0794576d1c26d0f0e
son <ngocsonkt99@gmail.com>
2022-01-15 00:03:44 +0700

    commit on main

---
+++ Toturial/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Vue</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="show = !show">Click</button>
+    <div v-show="show">Son Ung</div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Toturial/src/components/Search.vue
@@ -0,0 +1,11 @@
+<template>
+</template>
+html
+
+script js
+<script>
+</script>
+
+css
+<style>
+</style>
